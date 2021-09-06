ce99c5fe8f1997b4b104222cabd7a08988098795
rayouko234 <81438441+rayouko234@users.noreply.github.com>
2021-09-07 01:42:20 +0300

    Update settings.py

--- rokayproperties/settings.py
+++ rokayproperties/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 #    os.path.join(BASE_DIR,'rokayproperties/static')
 #]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
