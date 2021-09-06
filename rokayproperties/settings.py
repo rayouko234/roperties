755d405421375e3d0abc92aca6d0fe7fc3c1307d
rayouko234 <81438441+rayouko234@users.noreply.github.com>
2021-09-07 00:38:35 +0300

    Update settings.py

--- rokayproperties/settings.py
+++ rokayproperties/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rr$la69j7!s(%l1c+0j(om2e3ju&u67^74mignu-@t7317ju-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["rokayproperties.herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
